5b19a13a10bc321b1239ee900fe68280175587c8
JvsonKee <jasonk2723@gmail.com>
2024-04-24 02:26:46 -0600

    animated gradient

---
+++ client/src/components/Aura/Aura.styled.js
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+
+export const AuraContainer = styled.div`
+    overflow: hidden;
+
+    #up {
+        position: absolute;
+        height: 1000px;
+        width: 900px;
+        left: -100px;
+        border-radius: 50%;
+        background-image: linear-gradient(60deg, rgba(86, 128, 76, 1), rgba(58, 94, 88, 1));
+        filter: blur(80px);
+        animation: down 40s infinite;
+    }
+
+    #down {
+        position: absolute;
+        right: -120px;
+        height: 900px;
+        width: 900px;
+        border-radius: 50%;
+        background-image: linear-gradient(80deg, rgba(158, 173, 116, 0.5), rgba(152, 119, 57, 0.5));
+        filter: blur(80px);
+        animation: up 40s infinite;
+    }
+
+    #left {
+        position: absolute;
+        top: -400px;
+        height: 800px;
+        width: 900px;
+        border-radius: 50%;
+        background-image: linear-gradient(50deg, rgba(57, 152, 72, 1), rgba(124, 116, 173, 0.8));
+        filter: blur(80px);
+        animation: left 40s infinite;
+    }
+
+    #right {
+        position: absolute;
+        bottom: 100px;
+        height: 700px;
+        width: 900px;
+        border-radius: 50%;
+        background-image: linear-gradient(80deg, rgba(98, 105, 169, 0.5), rgba(166, 139, 97, 0.4));
+        filter: blur(80px);
+        animation: right 40s infinite;
+    }
+
+
+    @keyframes down {
+        0%, 100% {
+            top: 400px;
+        }
+
+        70% {
+            top: 500px;
+        }
+    }
+
+    @keyframes down {
+        0%, 100% {
+            bottom: 150px;
+        }
+
+        70% {
+            bottom: 100px;
+        }
+    }
+
+    @keyframes up {
+        0%, 100% {
+            bottom: -100px;
+        }
+
+        70% {
+            bottom: 500px;
+        }
+    }
+
+    @keyframes up {
+        0%, 100% {
+            top: -100px;
+        }
+
+        70% {
+            top: 150px;
+        }
+    }
+
+    @keyframes left {
+        0%, 100% {
+            right: -100px;
+        }
+
+        70% {
+            right: 1300px;
+        }
+    }
+
+    @keyframes left {
+        0%, 100% {
+            left: -100px;
+        }
+
+        70% {
+            left: 700px;
+        }
+    }
+
+    @keyframes right {
+        0%, 100% {
+            left: -100px;
+        }
+
+        70% {
+            left: 1300px;
+        }
+    }
+
+    @keyframes right {
+        0%, 100% {
+            right: -100px;
+        }
+
+        70% {
+            right: 700px;
+        }
+    }
+`
+
+export const Section = styled.section`
+
+`
