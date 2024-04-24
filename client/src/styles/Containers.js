41ce41e426648dff8e1510362f82f465bc3277f9
JvsonKee <jasonk2723@gmail.com>
2024-04-24 02:28:07 -0600

    page containers

---
+++ client/src/styles/Containers.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const PageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    min-height: 100vh;
+    background-image: linear-gradient(80deg, var(--dark-green), var(--dark-green));
+`
+
+export const ContentContainer = styled.div`
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+`
+
+export const ContentWrapper = styled.div`
+    display: flex;  
+    align-items: center;
+    flex-direction: column;
+    gap: 80px;
+    margin: 200px 0 50px 0;
+`
