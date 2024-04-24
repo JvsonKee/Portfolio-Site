8957f1ce2a68fed26b51b12e44a4c6badbaab344
JvsonKee <jasonk2723@gmail.com>
2024-04-24 12:24:46 -0600

    new dynamic rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react"
 import About from "./pages/Home/Home"
+import { ContentContainer, PageContainer } from "./styles/Containers";
+import Aura from "./components/Aura/Aura";
+import NavBar from "./components/NavBar/NavBar";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+
+	const [page, setPage] = useState("about");
+
+	function receiveData(data) {
+		setPage(data);
+	}
+
+	console.log({page})
+
 	return (
-		<About />
+		<PageContainer>
+			<Aura />
+			<ContentContainer>
+				<NavBar sendToParent={receiveData}/>
+				{
+					page === "about" ? <About />:
+					page === "projects" ? <Projects /> :
+					page === "contact" ? <Contact /> :
+					null
+				}
+				{
+					page === "about" || page === "projects" ? <Footer /> : null
+				}
+			</ContentContainer>
+		</PageContainer>
 	)
 }
 
