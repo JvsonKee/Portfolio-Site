91a8259680cbb5c37783f3923cf6b2d25d18a9a2
JvsonKee <jasonk2723@gmail.com>
2024-04-24 12:25:16 -0600

    removed react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,31 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyle from './styles/GlobalStyles.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Projects from './pages/Projects/Projects.jsx';
-import Contact from './pages/Contact/Contact.jsx';
-
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/projects',
-		element: <Projects />
-	},
-	{
-		path: '/contact',
-		element: <Contact />
-	}
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<div>
 		<GlobalStyle />
-		<RouterProvider router={router}/>
+		<App />
 	</div>
 )
